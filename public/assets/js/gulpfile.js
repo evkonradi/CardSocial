c72737b8f442204f09ba9b425cdb24d93f036efd
Jenniffer <jenlpac@yahoo.com>
2020-08-27 20:52:20 -0500

    add gulp functionality

---
+++ public/assets/js/gulpfile.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+//Compile: gulp.task(‘sass’, function () { gulp.src(‘app/scss/app.scss’) .pipe(sass().on(‘error’, sass.logError)) .pipe(gulp.dest(‘app/css’)); });
+
+//Compile and watch: gulp.task(‘sass:watch’, function() { gulp.watch(‘app/scss/app.scss’, [‘sass’]);});
+
