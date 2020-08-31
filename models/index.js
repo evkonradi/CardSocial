410d9c8d92e36d3315ce571bd2087bcf8125e0ae
Jenniffer <jenlpac@yahoo.com>
2020-08-31 18:31:56 -0500

    define model associations

--- models/index.js
+++ models/index.js
@@ -4,6 +4,36 @@ const Background = require('./background');
 const Icon = require('./icon');
 const Card = require('./card');
 
+User.hasMany(Card, {
+    foreignKey: 'user_id',
+});
 
+Card.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+Card.hasOne(Font, {
+    foreignKey: 'card_id',
+});
+
+Font.belongsTo(Card, {
+    foreignKey: 'card_id',
+});
+
+Card.hasOne(Background, {
+    foreignKey: 'card_id',
+});
+
+Background.belongsTo(Card, {
+    foreignKey: 'card_id',
+});
+
+Card.hasOne(Icon, {
+    foreignKey: 'card_id',
+});
+
+Icon.belongsTo(Card, {
+    foreignKey: 'card_id',
+});
 
 module.exports = { User, Font, Background, Icon, Card };
