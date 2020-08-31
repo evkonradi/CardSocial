da239f37b4cc996f04735a6a272c0897b177f1eb
Jenniffer <jenlpac@yahoo.com>
2020-08-31 12:22:19 -0500

    initialize and test card icon routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes.js');
 const fontRoutes = require('./card_font-routes');
 const backgroundRoutes = require('./card_background-routes');
+const iconRoutes = require('./card_icon-routes');
 
 router.use('/users', userRoutes);
 router.use('/fonts', fontRoutes);
 router.use('/backgrounds', backgroundRoutes);
+router.use('/icons', iconRoutes);
 
 module.exports = router;
