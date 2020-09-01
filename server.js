f2fe8015af7c379b16db48cc155c1ae18b66add6
ElK <62822011+evkonradi@users.noreply.github.com>
2020-09-01 15:29:03 -0500

    main.handlebars - working on nav menu

--- server.js
+++ server.js
@@ -31,15 +31,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-// const sess = {
-//     secret: 'Super secret secret',
-//     cookie: {},
-//     resave: false,
-//     saveUninitialized: true,
-//     store: new SequelizeStore({
-//       db: sequelize
-//     })
-//   };
 app.use(session(sess));
 
 app.use(routes);
