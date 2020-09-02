afa47928c56eba3f0a72f6f5e61992c31f48b376
ElK <62822011+evkonradi@users.noreply.github.com>
2020-09-02 14:00:46 -0500

    correcting reference to user in Card Model to be lower case

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,7 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    //res.render('homepage', {loggedIn: req.session.loggedIn});
-    res.render('homepage', {loggedIn: false});
+    res.render('homepage', {loggedIn: req.session.loggedIn});
 });
 
 router.get('/login', (req, res) => {
