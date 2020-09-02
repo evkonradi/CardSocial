7063e2e4f9f5ccfecd0688d5438d54500073e538
ElK <62822011+evkonradi@users.noreply.github.com>
2020-09-02 11:27:36 -0500

    change for testing in / route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.render('homepage', {loggedIn: req.session.loggedIn});
+    //res.render('homepage', {loggedIn: req.session.loggedIn});
+    res.render('homepage', {loggedIn: false});
 });
 
 router.get('/login', (req, res) => {
