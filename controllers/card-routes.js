e0904ed3d8c94dc69895a12457c75ea608070a08
ElK <62822011+evkonradi@users.noreply.github.com>
2020-09-02 15:26:58 -0500

    added route for displaying card by code

--- controllers/card-routes.js
+++ controllers/card-routes.js
@@ -1,7 +1,40 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { User, Card, Font, Icon, Background } = require('../models');
 
 router.get('/', (req, res) => {
-    res.render('usercard');
+
+    Card.findOne({
+        attributes: { exclude: ['pwd'] },
+        where: {
+          card_code: req.body.card_code
+        },
+        include: [
+            {
+                model: User,
+                attributes: { exclude: ['pwd'] }
+            },
+            {
+                model: Font
+            },
+            {
+                model: Icon
+            },
+            {
+                model: Background
+            },
+      
+        ]
+      })
+        .then(dbUserData => {
+            res.json(dbUserData);
+            //const card = dbUserData.get({ plain: true });
+            //res.render('usercard', {card});
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+        });
 });
 
 module.exports = router;
