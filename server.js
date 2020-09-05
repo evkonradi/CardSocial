00dbc716945222ea8c1c6cbe68ae47b0d0212418
ElK <62822011+evkonradi@users.noreply.github.com>
2020-09-05 13:22:06 -0500

    session timeout in 30 minutes

--- server.js
+++ server.js
@@ -14,9 +14,10 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: {maxAge: 1000 * 60 * 30}, // 30 minutes session timeout
   resave: false,
   saveUninitialized: true,
+  rolling: true,
   store: new SequelizeStore({
     db: sequelize
   })
