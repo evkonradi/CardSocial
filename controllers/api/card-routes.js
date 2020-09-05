f4b356e35129658fb85b083d8965d86f5813ea92
Jenniffer <jenlpac@yahoo.com>
2020-09-05 09:28:57 -0500

    update card code

--- controllers/api/card-routes.js
+++ controllers/api/card-routes.js
@@ -34,64 +34,66 @@ router.get('/:id', (req, res) => {
 
 // POST /api/cards
 router.post('/', (req, res) => {
-    Card.create({
-        card_code: createCode(),
-        card_title: req.body.card_title,
-        user_id: req.body.user_id,
-        font_id: req.body.font_id,
-        background_id: req.body.background_id,
-        icon_id: req.body.icon_id,
-        // card_code: req.body.card_code,
-        // include: [
-        //     {
-        //         model: User,
-        //         attributes: ['user_id']
-        //     }
-        // ],
-        // include: [
-        //     {
-        //         model: Font,
-        //         attributes: ['font_id']
-        //     }
-        // ],
-        // include: [
-        //     {
-        //         model: Background,
-        //         attributes: ['background_id']
-        //     }
-        // ],
-        // include: [
-        //     {
-        //         model: Icon,
-        //         attributes: ['icon_id']
-        //     }
-        // ],
-        show_first_name: req.body.show_first_name,
-        show_last_name: req.body.show_last_name,
-        show_nickname: req.body.show_nickname,
-        show_home_address: req.body.show_home_address,
-        show_business_name: req.body.show_business_name,
-        show_business_address: req.body.show_business_address,
-        show_position: req.body.show_position,
-        show_personal_phone: req.body.show_personal_phone,
-        show_business_phone: req.body.show_business_phone,
-        show_business_phone_ext: req.body.show_business_phone_ext,
-        show_personal_email: req.body.show_personal_email,
-        show_business_email: req.body.show_business_email,
-        show_junk_email: req.body.show_junk_email,
-        show_business_url: req.body.show_business_url,
-        show_linkedin_url: req.body.show_linkedin_url,
-        show_instagram_url: req.body.show_instagram_url,
-        show_facebook_url: req.body.show_facebook_url,
-        show_twitter_url: req.body.show_twitter_url,
-        show_bio: req.body.show_bio,
-        show_slogan: req.body.show_slogan
-    })
-        .then(dbCardData => res.json(dbCardData))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+    if (req.session) {
+        Card.create({
+            card_code: createCode(),
+            card_title: req.body.card_title,
+            user_id: req.session.user_id,
+            font_id: req.body.font_id,
+            background_id: req.body.background_id,
+            icon_id: req.body.icon_id,
+            // card_code: req.body.card_code,
+            // include: [
+            //     {
+            //         model: User,
+            //         attributes: ['user_id']
+            //     }
+            // ],
+            // include: [
+            //     {
+            //         model: Font,
+            //         attributes: ['font_id']
+            //     }
+            // ],
+            // include: [
+            //     {
+            //         model: Background,
+            //         attributes: ['background_id']
+            //     }
+            // ],
+            // include: [
+            //     {
+            //         model: Icon,
+            //         attributes: ['icon_id']
+            //     }
+            // ],
+            show_first_name: req.body.show_first_name,
+            show_last_name: req.body.show_last_name,
+            show_nickname: req.body.show_nickname,
+            show_home_address: req.body.show_home_address,
+            show_business_name: req.body.show_business_name,
+            show_business_address: req.body.show_business_address,
+            show_position: req.body.show_position,
+            show_personal_phone: req.body.show_personal_phone,
+            show_business_phone: req.body.show_business_phone,
+            show_business_phone_ext: req.body.show_business_phone_ext,
+            show_personal_email: req.body.show_personal_email,
+            show_business_email: req.body.show_business_email,
+            show_junk_email: req.body.show_junk_email,
+            show_business_url: req.body.show_business_url,
+            show_linkedin_url: req.body.show_linkedin_url,
+            show_instagram_url: req.body.show_instagram_url,
+            show_facebook_url: req.body.show_facebook_url,
+            show_twitter_url: req.body.show_twitter_url,
+            show_bio: req.body.show_bio,
+            show_slogan: req.body.show_slogan
+        })
+            .then(dbCardData => res.json(dbCardData))
+            .catch(err => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    }
 });
 
 // PUT /api/cards/1
