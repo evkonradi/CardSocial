2540e4b0a2b419e7d6c4800a3aaf06498277e569
Jenniffer <jenlpac@yahoo.com>
2020-09-05 11:26:48 -0500

    starting code to delete/edit

--- public/js/edit.js
+++ public/js/edit.js
@@ -1,12 +1,15 @@
-
-async function deleteCardHandler(event) {
-    event.preventDefault();
-    console.log("A delete button was pressed.")
+var editCardHandler = function(event) {
+    var targetEl = event.target;
+    if (targetEl.hasAttribute("data-delete-card-code")) {
+    console.log("A delete button was pressed.");
     // const response = await fetch('/api/cards', {
     //     method: 'delete',
     // })
-}
+    } else if (targetEl.hasAttribute("data-edit-card-code")) {
+        console.log("An edit button was pressed.");
+    }
+};
 
 
 
-document.querySelector('#cardDelete').addEventListener('click', deleteCardHandler);
+document.querySelector('#mycards-list').addEventListener("click", editCardHandler);
