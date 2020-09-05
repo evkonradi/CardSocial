c6fff7a9bc8649615ab34c78b4e5285ab4a755f3
Jenniffer <jenlpac@yahoo.com>
2020-09-05 01:00:56 -0500

    test delete button event listener

--- public/js/mycards.js
+++ public/js/mycards.js
@@ -66,5 +66,14 @@ async function saveCardHandler(event) {
     }
 };
 
+async function deleteCardHandler(event) {
+    event.preventDefault();
+    console.log("A delete button was pressed.")
+    // const response = await fetch('/api/cards', {
+    //     method: 'delete',
+    // })
+}
+
   
 document.querySelector('#cardSave').addEventListener('click', saveCardHandler);
+document.querySelector('#cardDelete').addEventListener('click', deleteCardHandler);
